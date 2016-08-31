2bab0b4f0952af4d5e15fa6d58b13a3aa7e8639e
程鹏飞 <dinkevin@163.com>
2016-08-31 16:41:47 +0800

    更新 DecimalUtil 类，添加加、减、乘、除等接口

--- XUI/src/com/dinkevin/xui/util/DecimalUtil.java
+++ XUI/src/com/dinkevin/xui/util/DecimalUtil.java
@@ -3,7 +3,7 @@ package com.dinkevin.xui.util;
 import java.math.BigDecimal;
 
 /**
- * 精确运算辅助类
+ * 精确运算辅助类，精确到小数点后两位
  * @author chengpengfei
  */
 public final class DecimalUtil {
@@ -11,15 +11,145 @@ public final class DecimalUtil {
 	private DecimalUtil(){}
 	
 	/**
-	 * 除
+	 * 加，默认是精确到小数后两位
 	 * @param a 被除数
 	 * @param b 除数
-	 * @return
+	 * @return 精确到小数点后两位的 double 数
+	 */
+	public static double add(double a,double b){
+		BigDecimal numberA = new BigDecimal(a);
+		BigDecimal numberB = new BigDecimal(b);
+		BigDecimal result = numberA.add(numberB).setScale(2);
+		return result.doubleValue();
+	}
+	
+	/**
+	 * 加，默认是精确到小数后两位
+	 * @param a 被除数
+	 * @param b 除数
+	 * @return 精确到小数点后两位的 double 数
+	 */
+	public static double add(long a,double b){
+		BigDecimal numberA = new BigDecimal(a);
+		BigDecimal numberB = new BigDecimal(b);
+		BigDecimal result = numberA.add(numberB).setScale(2);
+		return result.doubleValue();
+	}
+	
+	/**
+	 * 减，默认是精确到小数后两位
+	 * @param a 被除数
+	 * @param b 除数
+	 * @return 精确到小数点后两位的 double 数
+	 */
+	public static double subtract(double a,double b){
+		BigDecimal numberA = new BigDecimal(a);
+		BigDecimal numberB = new BigDecimal(b);
+		BigDecimal result = numberA.subtract(numberB).setScale(2);
+		return result.doubleValue();
+	}
+	
+	/**
+	 * 减，默认是精确到小数后两位
+	 * @param a 被除数
+	 * @param b 除数
+	 * @return 精确到小数点后两位的 double 数
+	 */
+	public static double subtract(double a,long b){
+		BigDecimal numberA = new BigDecimal(a);
+		BigDecimal numberB = new BigDecimal(b);
+		BigDecimal result = numberA.subtract(numberB).setScale(2);
+		return result.doubleValue();
+	}
+	
+	/**
+	 * 减，默认是精确到小数后两位
+	 * @param a 被除数
+	 * @param b 除数
+	 * @return 精确到小数点后两位的 double 数
+	 */
+	public static double subtract(long a,double b){
+		BigDecimal numberA = new BigDecimal(a);
+		BigDecimal numberB = new BigDecimal(b);
+		BigDecimal result = numberA.subtract(numberB).setScale(2);
+		return result.doubleValue();
+	}
+	
+	/**
+	 * 除，默认是精确到小数后两位
+	 * @param a 被除数
+	 * @param b 除数
+	 * @return 精确到小数点后两位的 double 数
 	 */
 	public static double divide(long a,long b){
 		BigDecimal numberA = new BigDecimal(a);
 		BigDecimal numberB = new BigDecimal(b);
 		BigDecimal result = numberA.divide(numberB,2,BigDecimal.ROUND_HALF_UP);
+		return result.doubleValue();
+	}
+	
+	/**
+	 * 除，默认是精确到小数后两位
+	 * @param a 被除数
+	 * @param b 除数
+	 * @return 精确到小数点后两位的 double 数
+	 */
+	public static double divide(long a,double b){
+		BigDecimal numberA = new BigDecimal(a);
+		BigDecimal numberB = new BigDecimal(b);
+		BigDecimal result = numberA.divide(numberB,2,BigDecimal.ROUND_HALF_UP);
+		return result.doubleValue();
+	}
+	
+	/**
+	 * 除，默认是精确到小数后两位
+	 * @param a 被除数
+	 * @param b 除数
+	 * @return 精确到小数点后两位的 double 数
+	 */
+	public static double divide(double a,long b){
+		BigDecimal numberA = new BigDecimal(a);
+		BigDecimal numberB = new BigDecimal(b);
+		BigDecimal result = numberA.divide(numberB,2,BigDecimal.ROUND_HALF_UP);
+		return result.doubleValue();
+	}
+	
+	/**
+	 * 除，默认是精确到小数后两位
+	 * @param a 被除数
+	 * @param b 除数
+	 * @return 精确到小数点后两位的 double 数
+	 */
+	public static double divide(double a,double b){
+		BigDecimal numberA = new BigDecimal(a);
+		BigDecimal numberB = new BigDecimal(b);
+		BigDecimal result = numberA.divide(numberB,2,BigDecimal.ROUND_HALF_UP);
+		return result.doubleValue();
+	}
+	
+	/**
+	 * 乘，默认是精确到小数后两位
+	 * @param a
+	 * @param b
+	 * @return 精确到小数点后两位的 double 数
+	 */
+	public static double multiply(long a,double b){
+		BigDecimal numberA = new BigDecimal(a);
+		BigDecimal numberB = new BigDecimal(b);
+		BigDecimal result = numberA.multiply(numberB).setScale(2);
+		return result.doubleValue();
+	}
+	
+	/**
+	 * 乘，默认是精确到小数后两位
+	 * @param a
+	 * @param b
+	 * @return 精确到小数点后两位的 double 数
+	 */
+	public static double multiply(double a,double b){
+		BigDecimal numberA = new BigDecimal(a);
+		BigDecimal numberB = new BigDecimal(b);
+		BigDecimal result = numberA.multiply(numberB).setScale(2);
 		return result.doubleValue();
 	}
 }
